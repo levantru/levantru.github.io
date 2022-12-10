3be5c660d6a90a394aa70b02173eded26eb3779d
levantru <letru.namdinh@gmail.com>
2022-12-10 11:21:55 +0700

    up

---
+++ js/slide.js
@@ -0,0 +1,45 @@
+$('.slider-images').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    nextArrow: '<span class="button-slide t-right"><i class="-ap icon-arrow-right "></i></span>',
+    prevArrow: '<span class="button-slide t-left"><i class="-ap icon-arrow-left "></i></span>',
+    responsive: [
+    {
+        breakpoint: 991,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+        }
+    },
+    {
+        breakpoint: 767,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }
+    ]
+});
+$( window ).on( "load", function() { 
+    setTimeout(function(){ 
+        $(".loading").addClass("hide");
+    }, 3000);
+    $('body').one('click',function(){
+        enableAutoplay();
+    });
+});
+    var vid = document.getElementById("myVideo");
+    function enableAutoplay() { 
+        vid.autoplay = true;
+        vid.load();
+   }
