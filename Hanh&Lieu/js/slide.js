7d4c847175aa0fe4afb1e20cd429240ed00d0214
levantru <61998582+levantru@users.noreply.github.com>
2023-02-21 19:07:47 +0700

    Add files via upload

---
+++ Hanh&Lieu/js/slide.js
@@ -0,0 +1,50 @@
+// $('.slider-images').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     arrows: true,
+//     autoplay: true,
+//     autoplaySpeed: 5000,
+//     nextArrow: '<span class="button-slide t-right"><i class="-ap icon-arrow-right "></i></span>',
+//     prevArrow: '<span class="button-slide t-left"><i class="-ap icon-arrow-left "></i></span>',
+//     responsive: [
+//     {
+//         breakpoint: 991,
+//         settings: {
+//             slidesToShow: 3,
+//             slidesToScroll: 1,
+//         }
+//     },
+//     {
+//         breakpoint: 767,
+//         settings: {
+//             slidesToShow: 2,
+//             slidesToScroll: 1
+//         }
+//     },
+//     {
+//         breakpoint: 480,
+//         settings: {
+//             slidesToShow: 1,
+//             slidesToScroll: 1
+//         }
+//     }
+//     ]
+// });
+  //  var vid = document.getElementById("myVideo");
+    //function enableAutoplay() { 
+       // vid.autoplay = true;
+      //  vid.load();
+  // }
+$( window ).on( "load", function() { 
+    setTimeout(function(){ 
+        $(".loading").addClass("hide");
+    }, 2000);
+   // if ($(window).width() >767) {
+     //   $('body').one('click',function(){
+       //     enableAutoplay();
+      //  });
+   //  }
+   //  else {
+      //  vid.autoplay = true;
+    // }
+});
